d3a5caa90ab5610c14e4edab461d304a66d88730
Elian Rojas <impresosgosen@gmail.com>
2021-01-25 13:43:16 -0600

    Version Final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,11 @@
-$(document).ready(function () {
 
-	$("h3,h4").dblclick(function () {
-	  $(this).css({
-		"color": "red",
-		"font-size": "2em"
-	  })
-	})
-	
-  })
-//   $(document).ready(function() {
-//     $("#mensaje_contacto").click(function() {
-//         alert("NO LLAME, NOSOTROS LO LLAMAMOS")
-//     })
-
-
-  $(document).ready(function() {
-    $("#enviarCorreo").click(function() {
-        alert("El correo fue enviado correctamente")
-    })
     $('.carousel').carousel()
     $(function() {
         $('[data-toggle="tooltip"]').tooltip()
     })
-    $('.ingredients').dblclick(function() {
-        $(this).css("color", "red")
-    })
-    $('.preparation').dblclick(function() {
-        $(this).css("color", "red")
-    })
-    $('.card-1').click(function() {
-        $('.card-11').toggle()
-    })
-    $('.card-2').click(function() {
-        $('.card-22').toggle()
-    })
-    $('.card-3').click(function() {
-        $('.card-33').toggle()
-    })
+    
 })
-})
+
 
     
     
